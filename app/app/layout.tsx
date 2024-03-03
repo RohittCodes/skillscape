296d9b5a4a1f20a376bf31d8d9f6638aa31355c6
Rohith Singh <singhmalik583@gmail.com>
2024-03-03 10:01:14 +0530

    auth redirect

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,24 +1,20 @@
 import { redirect } from "next/navigation";
 import Navbar from "@/components/globals/navbar";
 import { createClient } from "@/utils/supabase/server";
+import { User } from "@/types/user";
 
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
-  let userData = null;
-  const checkUser = async () => {
-    const supabase = createClient();
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    userData = user;
-    console.log(user);
-  };
+const MainLayout = async ({ children }: { children: React.ReactNode }) => {
+  let userData: User | null = null;
+
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  userData = user;
 
   if (!userData) {
     redirect("/auth/sign-in");
   }
-
-  // Call the checkUser function directly during component rendering
-  checkUser();
 
   return (
     <div>
--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,17 +2,13 @@ import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
-  const checkUser = async () => {
-    const supabase = createClient();
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    if (user) {
-      redirect("/app");
-    }
-  };
-
-  checkUser();
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (user) {
+    redirect("/app");
+  }
 
   return (
     <div className="flex h-full justify-center items-center">{children}</div>
