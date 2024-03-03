24a8dfd4fb8dd066294f320169e30474f1e7a0c0
Rohith Singh <singhmalik583@gmail.com>
2024-03-03 09:30:05 +0530

    auth redirect

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -3,15 +3,19 @@ import Navbar from "@/components/globals/navbar";
 import { createClient } from "@/utils/supabase/server";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  let userData = null;
   const checkUser = async () => {
     const supabase = createClient();
     const {
       data: { user },
     } = await supabase.auth.getUser();
-    if (!user) {
-      redirect("/auth/sign-in");
-    }
+    userData = user;
+    console.log(user);
   };
+
+  if (!userData) {
+    redirect("/auth/sign-in");
+  }
 
   // Call the checkUser function directly during component rendering
   checkUser();
